a5f06e11bc5dfb36c7fb23cd6d26e569ee33110e
Dani <dani@MacBook-Pro-de-Dani.local>
2024-10-29 00:02:24 +0100

    pylint issues in tests fixed

--- Sentiment_Analysis/tests/data_preprocessing_test.py
+++ Sentiment_Analysis/tests/data_preprocessing_test.py
@@ -1,7 +1,14 @@
+"""
+Unit tests for data preprocessing functions in the Sentiment_Analysis package.
+"""
+# pylint: disable=import-error
 from Sentiment_Analysis.data_preprocessing import clean_text
 
 
 def test_clean_text():
+    """
+    Test the clean_text function for correct text preprocessing.
+    """
     input_text = "This is a sample #text with https://example.com URL, numbers 123, and @mentions!"
     expected_output = "sampl text url number"
 
